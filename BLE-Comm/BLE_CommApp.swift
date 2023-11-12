d7c27cb776c116cb1eb0023328656cdac17e3c1a
raddevus <1radeutsch1@gmail.com>
2023-11-12 11:21:44 -0500

    Initial Commit

---
+++ BLE-Comm/BLE_CommApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLE_CommApp.swift
+//  BLE-Comm
+//
+//  Created by roger deutsch on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BLE_CommApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BLE-Comm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BLE-Comm
+//
+//  Created by roger deutsch on 11/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
