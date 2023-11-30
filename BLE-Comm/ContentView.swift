b0068de24b0ab3e534d48f736bc5a40d4c179a54
raddevus <1radeutsch1@gmail.com>
2023-11-30 18:41:35 -0500

    Got a working process for connecting & sending all at once via async methods and a wait loop.

--- BLE-Comm/ContentView.swift
+++ BLE-Comm/ContentView.swift
@@ -9,16 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
 
-    @ObservedObject private var gameTowerVM = GameTowerViewModel()
+    @State private var gameTowerVM = GameTowerViewModel()
     @State private var connectionBtnText = "Connect"
     @State private var charCount = 0
     @State private var pickerVal :String = ""
+    @State private var isBleConnected: Bool = false
     
     @State private var dataToSend = ""
+    @State private var isSendAlertDisplayed = false
+    
+    func setBleConnectionState(_ isConnected: Bool){
+        isBleConnected = isConnected
+    }
     
     init(){
         print ("in init...")
         pickerVal = gameTowerVM.allCharacteristics[0]
+        
     }
     
     var body: some View {
@@ -30,7 +37,10 @@ struct ContentView: View {
                     Button(connectionBtnText){
                         if (gameTowerVM.connectionState == false){
                             connectionBtnText = "Disconnect"
-                            gameTowerVM.connect()}
+                            Task{
+                                await gameTowerVM.connect(setBleConnectionState)
+                            }
+                        }
                         else{
                             connectionBtnText = "Connect"
                             gameTowerVM.disconnect()
@@ -39,27 +49,32 @@ struct ContentView: View {
                     }.buttonStyle(.bordered)
                 }
                 TextField("Current state: ", text:$gameTowerVM.connectionStateText)
-                Picker("Chars", selection: $charCount){
-                    
-                    ForEach (gameTowerVM.allCharacteristics.indices, id: \.self)
-                    {
-                        Text(gameTowerVM.allCharacteristics[$0])
-                    }
-                    
-                }
-                Picker("istics", selection: $pickerVal){
-                    ForEach(gameTowerVM.allCharacteristics, id:\.self){
-                        Text($0)
-                    }
-                }
+//                Picker("Chars", selection: $charCount){
+//                    
+//                    ForEach (gameTowerVM.allCharacteristics.indices, id: \.self)
+//                    {
+//                        Text(gameTowerVM.allCharacteristics[$0])
+//                    }
+//                    
+//                }
+//                Picker("istics", selection: $pickerVal){
+//                    ForEach(gameTowerVM.allCharacteristics, id:\.self){
+//                        Text($0)
+//                    }
+//                }
                 HStack{
                     TextField("text to send",text:$dataToSend )
                     
                     Button("Send"){
-                        gameTowerVM.sendData(data: dataToSend)
+                        task{
+                            await gameTowerVM.sendData(data: dataToSend)
+                        }
+                        
+                        
                         //dataToSend = ""
                         
                     }.buttonStyle(.bordered)
+                    
                 }
             }
             .padding()
@@ -67,12 +82,47 @@ struct ContentView: View {
                 Label("Main", systemImage:"star")
             }.tag("main")
             VStack{
-                Text("tab 2")
+                Button("Send Random"){
+                    Task{
+                        await gameTowerVM.connect(setBleConnectionState)
+                        while(!isBleConnected){
+                            Thread.sleep(forTimeInterval: 0.25)
+                        }
+                        await gameTowerVM.sendData(data: "what is up?! \(Date.now.formatted())")
+                        
+                    }
+                        //isSendAlertDisplayed = true
+                }
+                .alert("Data send successful!", isPresented: $isSendAlertDisplayed){
+                    Button("OK"){
+                        isSendAlertDisplayed = false
+                    }
+                }
+                Button("Disconnect"){
+                    isBleConnected = false
+                    gameTowerVM.disconnect()
+                }.buttonStyle(.bordered)
             }.tabItem{
                 Label("Config", systemImage:"circle")
             }.tag("config")
+            
         }
+        
     }
+    
+    func bleConnect(){
+        
+            if (gameTowerVM.connectionState){
+                return
+            }
+            else{
+                print("in ELSE...")
+                Task{
+                    await gameTowerVM.connect(setBleConnectionState)
+                }
+            }
+        }
+   
 }
 
 #Preview {
--- BLE-Comm/GameTowerViewModel.swift
+++ BLE-Comm/GameTowerViewModel.swift
@@ -19,6 +19,7 @@ class GameTowerViewModel : NSObject, ObservableObject, Identifiable{
     private var centralQueue: DispatchQueue?
     @Published var allCharacteristics : [String] = ["super", "ugly", "yikes"]
     private var inputCharacteristic : CBCharacteristic? = nil
+    private var setConnState: ((Bool) ->())? = nil
     
     func changeConnectionState(){
         connectionState = !connectionState
@@ -30,8 +31,9 @@ class GameTowerViewModel : NSObject, ObservableObject, Identifiable{
         
     }
     
-    func connect(){
+    func connect(_ setBleConnState: @escaping (Bool)->()) async {
         connectionState = true
+        setConnState = setBleConnState
         DispatchQueue.main.async{
             self.connectionStateText = "Connecting..."
         }
@@ -42,6 +44,7 @@ class GameTowerViewModel : NSObject, ObservableObject, Identifiable{
     func disconnect(){
         guard let manager = centralManager,
               let peripheral = connectedPeripheral else {return}
+        print("DISCONNECTING ....----->")
         manager.cancelPeripheralConnection(peripheral)
         connectionState = false
         DispatchQueue.main.async{
@@ -49,10 +52,19 @@ class GameTowerViewModel : NSObject, ObservableObject, Identifiable{
         }
     }
     
-    func sendData(data: String){
+    func sendData(data: String) async {
+        let sendMsg = data.data(using: String.Encoding.utf8)
+        print("sending data: \(data)")
+        print("input char is: \(inputCharacteristic?.uuid.uuidString ?? "NOPE!")")
+        
+        connectedPeripheral?.writeValue(sendMsg ?? Data(), for: inputCharacteristic!, type: .withoutResponse)
+    }
+    
+    func sendDataAndDisconnect(data: String){
         let sendMsg = data.data(using: String.Encoding.ascii)
         print("sending data: \(data)")
         connectedPeripheral?.writeValue(sendMsg ?? Data(), for: inputCharacteristic!, type: .withoutResponse)
+        disconnect()
     }
     
 }
@@ -63,15 +75,17 @@ extension GameTowerViewModel: CBCentralManagerDelegate{
             DispatchQueue.main.async{
                 self.connectionStateText = "Scanning..."
             }
+            // if you use nil instead of serviceUUID then this the scan will
+            // scan all available peripherals (within distance)
             central.scanForPeripherals(withServices: [serviceUUID])
         }
     }
     
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
         DispatchQueue.main.async{
-            self.connectionStateText = "Discovered \(peripheral.name ?? "UNKNOWN")"
+            print("Discovered \(peripheral.name ?? "UNKNOWN")")
         }
-        central.stopScan()
+        //central.stopScan()
         connectedPeripheral = peripheral
         central.connect(peripheral, options:nil)
     }
@@ -109,11 +123,14 @@ extension GameTowerViewModel : CBPeripheralDelegate{
             if ch.uuid.uuidString == "622B2C55-7914-4140-B85B-879C5E252DA0"{
                 print("gotcha!!!")
                 inputCharacteristic = ch
+                print("got it! \(ch.uuid.uuidString)")
+                setConnState!(true)
+                return
             }
             print("char \(ch.uuid.uuidString)")
-            DispatchQueue.main.async{
-                self.allCharacteristics.append(ch.uuid.uuidString)
-            }
+//            DispatchQueue.main.async{
+//                self.allCharacteristics.append(ch.uuid.uuidString)
+//            }
         }
     }
     
